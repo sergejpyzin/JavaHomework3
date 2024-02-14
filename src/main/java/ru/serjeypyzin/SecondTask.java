94fd689f2e863a4baba83e3b6560d4f3507b53d5
Sergej Pyzin <sergejpyzin@yandex.ru>
2024-02-14 17:16:13 +0300

    Create SecondTask

---
+++ src/main/java/ru/serjeypyzin/SecondTask.java
@@ -0,0 +1,22 @@
+package ru.serjeypyzin;
+/*
+Напишите обобщенный метод compareArrays(), который принимает два массива и возвращает true, если они одинаковые,
+ и false в противном случае. Массивы могут быть любого типа данных, но должны иметь одинаковую длину
+ и содержать элементы одного типа.
+ */
+
+public class SecondTask {
+
+    public static <T> boolean compareArrays (T[] firstSomeArray, T[] secondSomeArray){
+        if (firstSomeArray.length != secondSomeArray.length){
+            return false;
+        }
+
+        for (int i = 0; i < firstSomeArray.length; i++) {
+            if (firstSomeArray[i].getClass().getTypeName() != secondSomeArray[i].getClass().getTypeName()){
+                return false;
+            }
+        }
+        return true;
+    }
+}
